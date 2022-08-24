d3c047606d7d93b21ff80441bcb57edc13f0c24e
nekonlu <appyoohoy@gmail.com>
2022-08-24 17:30:10 +0900

    first commit

---
+++ WebAPI_00/WebAPI_00/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WebAPI_00
+//
+//  Created by nekonlu on 2022/08/24.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebAPI_00/WebAPI_00/WebAPI_00App.swift
@@ -0,0 +1,18 @@
+//
+//  WebAPI_00App.swift
+//  WebAPI_00
+//
+//  Created by nekonlu on 2022/08/24.
+//  
+//
+
+import SwiftUI
+
+@main
+struct WebAPI_00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
